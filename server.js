ce3787369e2786683b50f0e80093416e75392a40
jackass7__ <nobodyrecordslabel@gmail.com>
2026-05-08 09:53:09 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,21 +6,33 @@ const io = require('socket.io')(http, { cors: { origin: "*", methods: ["GET", "P
 app.get('/', (req, res) => { res.send('Serveur Boomerang OK'); });
 
 let playersCount = 0;
+let scores = { p1: 0, p2: 0 }; // Initialisation des scores
 
 io.on('connection', (socket) => {
     playersCount++;
     let role = (playersCount % 2 !== 0) ? 'p1' : 'p2';
-    socket.emit('init', role);
+    socket.emit('init', { role, scores }); // Envoie le rôle et le score actuel
 
     socket.on('playerState', (data) => { socket.broadcast.emit('enemyState', data); });
     socket.on('throwBoomerang', () => { socket.broadcast.emit('enemyThrow'); });
-    socket.on('playerDied', (data) => { socket.broadcast.emit('enemyDied', data); });
+    
+    socket.on('playerDied', (data) => {
+        // L'attaquant gagne un point
+        let winner = (data.victim === 'p1') ? 'p2' : 'p1';
+        scores[winner]++;
 
-    // --- NOUVEAU : RELAIS DU CHAT ---
-    socket.on('chatMessage', (msg) => {
-        socket.broadcast.emit('chatMessage', msg);
+        // On envoie le nouveau score et l'info du mort
+        io.emit('scoreUpdate', scores);
+        socket.broadcast.emit('enemyDied', data);
+
+        // Si quelqu'un atteint 10 victoires
+        if (scores[winner] >= 10) {
+            io.emit('gameWin', { winner });
+            scores = { p1: 0, p2: 0 }; // On reset pour la prochaine fois
+        }
     });
 
+    socket.on('chatMessage', (msg) => { socket.broadcast.emit('chatMessage', msg); });
     socket.on('disconnect', () => { playersCount--; });
 });
 
