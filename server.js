b2b5f35060f633e3fe36e0af30da6b5a7b13bfe8
jackass7__ <nobodyrecordslabel@gmail.com>
2026-05-09 03:20:51 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,35 +3,33 @@ const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http, { cors: { origin: "*", methods: ["GET", "POST"] } });
 
-app.get('/', (req, res) => { res.send('Serveur Survie Pro OK'); });
+app.get('/', (req, res) => { res.send('Serveur Survie HOTE OK'); });
 
-// Le serveur décide du monde une seule fois au démarrage
 const serverStartTime = Date.now();
-const worldSeeds = {
-    x: Math.random() * 1000000,
-    z: Math.random() * 1000000
-};
-
+const worldSeeds = { x: Math.random() * 1000000, z: Math.random() * 1000000 };
 const players = {};
+let destroyedObjects = [];
 
-// 🟢 NOUVEAU 1/3 : Le grand livre mémoire du monde
-let destroyedObjects = []; 
+// 🟢 NOUVEAU : Identifiant du joueur "Maître du Jeu"
+let hostId = null; 
 
 io.on('connection', (socket) => {
-    // Attribution du rôle et stockage
-    players[socket.id] = { 
-        id: socket.id, 
-        x: 0, y: 30, z: 0, ry: 0, 
-        color: Math.random() * 0xffffff // Couleur aléatoire pour chaque joueur
-    };
+    players[socket.id] = { id: socket.id, x: 0, y: 30, z: 0, ry: 0 };
 
-    // Envoi des Seeds, du temps, et de la mémoire
+    // Si personne n'est le chef, le nouveau devient le chef !
+    if (!hostId) {
+        hostId = socket.id;
+        console.log(`Nouvel Hôte assigné: ${hostId}`);
+    }
+
+    // On prévient le joueur s'il est l'Hôte au moment de l'initialisation
     socket.emit('init', { 
         id: socket.id, 
         players, 
         seeds: worldSeeds,
         startTime: serverStartTime,
-        destroyedObjects: destroyedObjects // 🟢 NOUVEAU 2/3 : On envoie la liste aux nouveaux
+        destroyedObjects: destroyedObjects,
+        isHost: (socket.id === hostId) // <-- INFO CRUCIALE
     });
     
     io.emit('playerCount', Object.keys(players).length);
@@ -44,21 +42,45 @@ io.on('connection', (socket) => {
         }
     });
 
-    // 🟢 NOUVEAU 3/3 : Quand un joueur détruit un truc (Arbre, Rocher, Coffre)
     socket.on('objectDestroyed', (objectId) => {
         if (!destroyedObjects.includes(objectId)) {
-            destroyedObjects.push(objectId); // Le serveur s'en souvient pour toujours
-            socket.broadcast.emit('objectDestroyed', objectId); // Il prévient les autres joueurs en direct
+            destroyedObjects.push(objectId); 
+            socket.broadcast.emit('objectDestroyed', objectId); 
         }
     });
 
-    // Relais du Chat (identique au prototype)
+    // 🟢 NOUVEAU : Relais des positions des monstres (De l'Hôte vers les Clients)
+    socket.on('syncEntities', (entitiesData) => {
+        if (socket.id === hostId) { // Sécurité : seul l'hôte a le droit de bouger les monstres
+            socket.broadcast.emit('syncEntities', entitiesData);
+        }
+    });
+
+    // 🟢 NOUVEAU : Un client frappe un monstre (Du Client vers l'Hôte)
+    socket.on('hitEntity', (data) => {
+        // On envoie la demande de dégâts directement à l'Hôte pour qu'il calcule la mort
+        io.to(hostId).emit('hitEntity', data);
+    });
+
     socket.on('chatMessage', (msg) => {
         socket.broadcast.emit('chatMessage', { text: msg, id: socket.id });
     });
 
     socket.on('disconnect', () => {
         delete players[socket.id];
+
+        // 🟢 NOUVEAU : Transfert de pouvoir si l'Hôte quitte la partie
+        if (socket.id === hostId) {
+            const remainingPlayers = Object.keys(players);
+            if (remainingPlayers.length > 0) {
+                hostId = remainingPlayers[0]; // Le joueur suivant devient l'hôte
+                io.to(hostId).emit('hostAssigned'); // On lui annonce la nouvelle
+                console.log(`Changement d'Hôte: ${hostId}`);
+            } else {
+                hostId = null; // Plus personne
+            }
+        }
+
         io.emit('playerLeft', socket.id);
         io.emit('playerCount', Object.keys(players).length);
     });
